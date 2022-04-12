749a54625ea0dfbbd649f6c5565a5e9eb1004a67
Chiko Shimizu <chikoski@google.com>
2022-04-12 17:31:36 +0900

    initial commit
    Change-Id: I8d7cdeb8664fba6b9e213711eb5b83011c3c82e0

---
+++ README.md
@@ -0,0 +1,27 @@
+# Sports App
+
+Starter code for "Add adaptive layouts to a view-based Android app with Compose" codelab. 
+
+Sports app is a basic app to introduce intermediate developers to adaptive layouts. 
+This app consists of three main screens: the sports list, favorites, and settings. 
+The favorites and settings screens shows placeholder texts. 
+This sports app shows a sports news (with a placeholder text) by selecting a sport in the 
+sports list.
+
+## Pre-requisites
+
+You need to know:
+- Basic knowledge of view-based UI development. 
+- Experience with Kotlin syntax, including lambda functions.
+- [Jetpack Compose basics codelab](https://developer.android.com/codelabs/jetpack-compose-basics).
+
+
+## Getting Started
+
+1. Download and run the app.
+
+## Acknowledgement
+
+This sports app is based on the 
+[code for "Adaptive Layouts" codelab](https://github.com/google-developer-training/basic-android-kotlin-training-sports), 
+which is a part of [Android Basics in Kotlin](https://developer.android.com/courses/android-basics-kotlin/course).
---
+++ app/src/main/java/com/example/android/sports/MainActivity.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.sports
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.toComposeRect
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+import androidx.window.layout.WindowMetricsCalculator
+import com.example.android.sports.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        binding.navigation.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) ||
+            super.onSupportNavigateUp()
+    }
+}
+
+enum class WidthSizeClass { COMPACT, MEDIUM, EXPANDED }
+
+@Composable
+fun Activity.rememberWidthSizeClass(): WidthSizeClass {
+    val configuration = LocalConfiguration.current
+    val windowMetrics = remember(configuration) {
+        WindowMetricsCalculator.getOrCreate()
+            .computeCurrentWindowMetrics(this)
+    }
+    val windowDpSize = with(LocalDensity.current) {
+        windowMetrics.bounds.toComposeRect().size.toDpSize()
+    }
+    return when {
+        windowDpSize.width < 600.dp -> WidthSizeClass.COMPACT
+        windowDpSize.width < 840.dp -> WidthSizeClass.MEDIUM
+        else -> WidthSizeClass.EXPANDED
+    }
+}
